55b0d613780361464afe2b852bf20e60663585af
Christian Bartenstein <christian.bartenstein@googlemail.com>
2026-02-12 17:10:51 +0100

    format code base with prettier

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # Media Speed Control
+
 This extension allows users to adjust the playback rate of HTML5 video and audio elements. It also features a _strict mode_ which enables users to overwrite the default behaviour of media players that might try to reset the playback rate. This extension works for Chromium based browsers and Mozilla Firefox.
 
 ## Setup
-* Chromium based browsers (Google Chrome, Microsoft Edge, Opera, Vivaldi, etc.):
-    1. Type `chrome://extensions` in the address bar. (If you use a browser other than Google Chrome, it will redirect you automatically.) 
-    2. Enable the __Developer mode__ temporarily.
-    3. Click __Load unpacked extension...__ and select the extension's folder.
 
-* Firefox:
-    1. Delete the `manifest.json` file and rename `manifest_firefox.json` to `manifest.json`.
-    2. Type `about:debugging#/runtime/this-firefox` in the address bar.
-    3. Click __Load Temporary Add-on__ and select the `manifest.json` from the extension's folder.
+- Chromium based browsers (Google Chrome, Microsoft Edge, Opera, Vivaldi, etc.):
+  1. Type `chrome://extensions` in the address bar. (If you use a browser other than Google Chrome, it will redirect you automatically.)
+  2. Enable the **Developer mode** temporarily.
+  3. Click **Load unpacked extension...** and select the extension's folder.
 
-    _Remarkable Notes:_ Extensions which are installed this way only remain installed until Firefox restarts. Also, Firefox only supports sound for playback rates which are not higher than 4 times the regular playback rate.
+- Firefox:
+  1. Delete the `manifest.json` file and rename `manifest_firefox.json` to `manifest.json`.
+  2. Type `about:debugging#/runtime/this-firefox` in the address bar.
+  3. Click **Load Temporary Add-on** and select the `manifest.json` from the extension's folder.
+
+  _Remarkable Notes:_ Extensions which are installed this way only remain installed until Firefox restarts. Also, Firefox only supports sound for playback rates which are not higher than 4 times the regular playback rate.
 
 ## TODOs/ ideas for possible future implementations
-* add options page to change and save personal settings
-* add keylisteners to control the playback rate
+
+- add options page to change and save personal settings
+- add keylisteners to control the playback rate
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,86 +1,90 @@
-let inputContainer;
-let noMediaContainer;
-let slider;
-let feedback;
-let checkbox;
+let inputContainer
+let noMediaContainer
+let slider
+let feedback
+let checkbox
 
 document.addEventListener('DOMContentLoaded', () => {
-    // initialize variables
-    inputContainer = document.querySelector("#inputContainer");
-    noMediaContainer = document.querySelector("#noMediaContainer");
-    slider = document.querySelector("#inputSlider");
-    feedback = document.querySelector("#rateFeedback");
-    checkbox = document.querySelector("#checkbox");
+  // initialize variables
+  inputContainer = document.querySelector('#inputContainer')
+  noMediaContainer = document.querySelector('#noMediaContainer')
+  slider = document.querySelector('#inputSlider')
+  feedback = document.querySelector('#rateFeedback')
+  checkbox = document.querySelector('#checkbox')
 
-    //get the current playback rate + strict mode setting and display it after click on icon
-    updatePopup();
+  //get the current playback rate + strict mode setting and display it after click on icon
+  updatePopup()
 
-    //wait for userinput
-    slider.addEventListener("input", inputHandler);
-    checkbox.addEventListener("input", inputHandler);
-});
+  //wait for userinput
+  slider.addEventListener('input', inputHandler)
+  checkbox.addEventListener('input', inputHandler)
+})
 
 function inputHandler() {
-    let rate = slider.value;
-    let strictMode = checkbox.checked;
+  let rate = slider.value
+  let strictMode = checkbox.checked
 
-    // save strictMode setting in storage
-    setInStorage("strictMode", strictMode);
+  // save strictMode setting in storage
+  setInStorage('strictMode', strictMode)
 
-    // send query to active tab
-    chrome.tabs.query({
-            currentWindow: true,
-            active: true
-        },
-        tabs => {
-            let msg = {
-                type: "input",
-                rate: rate,
-                strictMode: strictMode
-            };
-            chrome.tabs.sendMessage(tabs[0].id, msg);
+  // send query to active tab
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      active: true
+    },
+    (tabs) => {
+      let msg = {
+        type: 'input',
+        rate: rate,
+        strictMode: strictMode
+      }
+      chrome.tabs.sendMessage(tabs[0].id, msg)
 
-            //update popup view
-            updatePopup();
-        }
-    );
+      //update popup view
+      updatePopup()
+    }
+  )
 }
 
 function updatePopup() {
-    chrome.tabs.query({
-            currentWindow: true,
-            active: true
-        },
-        tabs => {
-            let msg = {
-                type: "update",
-            };
-            // not particualarly happy with this solution
-            chrome.tabs.sendMessage(tabs[0].id, msg, response => {
-                if (chrome.runtime.lastError) { // no response was sent -> no media found
-                    inputContainer.style.display = "none";
-                    noMediaContainer.style.display = "block";
-                } else { // media was found
-                    slider.value = response.currentRate;
-                    feedback.innerHTML = response.currentRate;
-                    getFromStorage("strictMode", strictMode => checkbox.checked = strictMode || false);
-                    inputContainer.style.display = "block";
-                    noMediaContainer.style.display = "none";
-                }
-            });
+  chrome.tabs.query(
+    {
+      currentWindow: true,
+      active: true
+    },
+    (tabs) => {
+      let msg = {
+        type: 'update'
+      }
+      // not particualarly happy with this solution
+      chrome.tabs.sendMessage(tabs[0].id, msg, (response) => {
+        if (chrome.runtime.lastError) {
+          // no response was sent -> no media found
+          inputContainer.style.display = 'none'
+          noMediaContainer.style.display = 'block'
+        } else {
+          // media was found
+          slider.value = response.currentRate
+          feedback.innerHTML = response.currentRate
+          getFromStorage('strictMode', (strictMode) => (checkbox.checked = strictMode || false))
+          inputContainer.style.display = 'block'
+          noMediaContainer.style.display = 'none'
         }
-    );
+      })
+    }
+  )
 }
 
 // some helper functions to deal with the chrome storage
 function setInStorage(key, data) {
-    chrome.storage.sync.set(JSON.parse(`{"${key}": ${data}}`), () => {
-        if (chrome.runtime.lastError) {
-            console.error("Could not set " + key + ": " + chrome.runtime.lastError.message);
-        }
-    });
+  chrome.storage.sync.set(JSON.parse(`{"${key}": ${data}}`), () => {
+    if (chrome.runtime.lastError) {
+      console.error('Could not set ' + key + ': ' + chrome.runtime.lastError.message)
+    }
+  })
 }
 
 function getFromStorage(key, callback) {
-    chrome.storage.sync.get([key], result => callback(result[key]));
+  chrome.storage.sync.get([key], (result) => callback(result[key]))
 }
